6dabb263307c06e20b8b73089e48b7fc3eb931bf
Felipe Oliveira <felipe.silva190910@gmail.com>
2026-09-22 21:18:19 -0300

    .

--- main.c
+++ main.c
@@ -13,20 +13,74 @@ main() {
 
     struct Aluno aluno[30];
     int quantidade_Alunos;
+    int opcao;
+    int total_Alunos = 0;
+do {
+
+    printf("\n=========================\n       INICIO \n=========================\n");
+    printf("1 - CADASTRAR ALUNOS");
+    printf("2 - VER ALUNOS CADASTRADOS");
+    printf("3 - VER APROVADOS/REPROVADOS");
+    printf("4 - VER BOLETIM");
+    printf("5 - ENCERRAR SISTEMA");
+    
 
 
+    switch (opcao)
+    {
+    case 1:
     printf("\n=========================\n       CADASTRO \n=========================\n");
 
     printf("Quantos alunos deseja cadastrar?\n");
-    printf("Resposta: ");
+    printf("Resposta: ");   
     scanf("%d", &quantidade_Alunos);
+    total_Alunos = quantidade_Alunos + total_Alunos;
 
-    for (int i = 0; i <= quantidade_Alunos; i++)
+    for (int i = 1; i <= quantidade_Alunos; i++)
     {
-    printf("\n--- ALUNO %d ---\n", i + 1);    
-    printf("Qual é o nome do aluno: \n");
-    printf("Resposta: ");
-    scanf("%49s", aluno[i].nome);
+        printf("\n--- ALUNO %d ---\n", i );    
+        printf("Nome do aluno:");
+        scanf("%49s", aluno[i].nome);
+        printf("Primeira nota: ");
+        scanf("%f", &aluno[i].nota1);
+        printf("Segunda nota: ");
+        scanf("%f", &aluno[i].nota2);
+        printf("Terceira nota: ");
+        scanf("%f", &aluno[i].nota3);
+        printf("Quarta nota: ");
+        scanf("%f", &aluno[i].nota4);
+        aluno[i].media = (aluno[i].nota1 + aluno[i].nota2 + aluno[i].nota3 + aluno[i].nota4) / 4;
+    }
+        
+    break;
+    
+
+    case 2: 
+    if(total_Alunos == 0) {
+        printf("Não temos alunos cadastro no momento!");
+    }
+
+    else{
+        for (int i = 1; i > total_Alunos; i++)
+        {
+            printf("Aluno %d: %s\n", i, aluno[i].nome);
+        }
+        
+    }
+
+    case 3: 
+        if(total_Alunos == 0) {
+            printf("Não temos alunos cadastro no momento!");
+        }
+        else{
+            
+        }
+
+
+    default:
+        break;
     }
     
+} while(opcao != 5);
+
 }
